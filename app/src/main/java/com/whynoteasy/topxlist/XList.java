5c779dcd814b8eaced33c25867a2206d5f9b97b7
AaronSpieler <spieleraaron@protonmail.com>
2017-10-31 20:36:26 +0100

    First Commit Ever :)

---
+++ app/src/main/java/com/whynoteasy/topxlist/XList.java
@@ -0,0 +1,66 @@
+package com.whynoteasy.topxlist;
+
+/**
+ * THESE ARE THE LISTS
+ * Created by Whatever on 27.10.2017.
+ */
+
+public class XList {
+    //Constants
+    final int xLTlength = 255; //max Title length
+    final int xLSDlength = 2048; //max short description length
+    final int xLLDlength = 8192; //max long description length
+
+    //Attributes
+    private int xListID;
+    private String xListTitle;
+    private String xListShortDescription;
+    private String xListLongDescription;
+    private int xListNum;
+
+    /*future Attributes
+    private String xListTags;
+    private int xListPrivacySetting;
+    private int xListLanguageID
+    */
+
+    public XList(int ID, String title, String shortDescription, String longDescription, int num){
+        xListID = ID;
+        xListTitle = title;
+        xListShortDescription = shortDescription;
+        xListLongDescription = longDescription;
+        xListNum = num;
+    }
+
+
+    public String getxListTitle(){
+        return xListTitle;
+    }
+    public void setxListTitle(String title){
+        xListTitle = title;
+    }
+
+    public String getxListShortDescription(){
+        return xListShortDescription;
+    }
+    public void setxListShortDescription(String shortDescription){
+        xListShortDescription = shortDescription;
+    }
+
+    public String getxListLongDescription(){
+        return xListLongDescription;
+    }
+    public void setxListLongDescription(String longDescription){
+        xListLongDescription = longDescription;
+    }
+
+    public int getxListNum(){
+        return xListNum;
+    }
+    public void setxListNum(int num){
+        xListNum = num;
+    }
+
+    public int getxListID() { return xListID; }
+    public void setxListID(int ID) {xListID = ID;}
+}
---
+++ app/src/main/java/com/whynoteasy/topxlist/XTag.java
@@ -0,0 +1,29 @@
+package com.whynoteasy.topxlist;
+
+/**
+ * Created by Whatever on 31.10.2017.
+ */
+
+public class XTag {
+    private int xTagID;
+    private String xTagName;
+
+    public XTag(int ID, String Name){
+        xTagID = ID;
+        xTagName = Name;
+    }
+
+    public int getxTagID() {
+        return xTagID;
+    }
+    public String getxTagName() {
+        return xTagName;
+    }
+
+    public void setxTagID(int ID) {
+        xTagID = ID;
+    }
+    public void setxTagName(String name) {
+        xTagName = name;
+    }
+}
