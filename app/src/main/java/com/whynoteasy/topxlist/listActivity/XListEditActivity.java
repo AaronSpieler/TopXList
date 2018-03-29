7a3362ac535b18488946054c04bf3807f86f235d
AaronSpieler <spieleraaron@protonmail.com>
2018-03-29 17:28:13 +0200

    Lists are now editable

--- app/src/main/java/com/whynoteasy/topxlist/listActivity/XListEditActivity.java
+++ app/src/main/java/com/whynoteasy/topxlist/listActivity/XListEditActivity.java
@@ -1,19 +1,14 @@
 package com.whynoteasy.topxlist.listActivity;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.NavUtils;
-import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +20,7 @@ import android.widget.TextView;
 import com.whynoteasy.topxlist.R;
 import com.whynoteasy.topxlist.data.LocalDataRepository;
 import com.whynoteasy.topxlist.object.XListModel;
+import com.whynoteasy.topxlist.object.XListTagsPojo;
 import com.whynoteasy.topxlist.object.XTagModel;
 
 import java.util.ArrayList;
@@ -33,10 +29,17 @@ import java.util.List;
 import static android.support.design.widget.Snackbar.LENGTH_SHORT;
 
 public class XListEditActivity extends AppCompatActivity {
+    private XListTagsPojo currentList;
+    private int currentListID;
+    private LocalDataRepository myRep;
 
-    List<String> tempTagList = new ArrayList<String>();
-    EditText tagEditText;
-    Activity thisActivity;
+    private List<String> tempTagListNew = new ArrayList<String>();
+    private List<Integer> tempTagListDeleted = new ArrayList<Integer>();
+
+    private ViewGroup insertPoint;
+    private EditText tagEditText;
+
+    private Activity thisActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,18 +48,63 @@ public class XListEditActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_xlist_edit);
         setSupportActionBar(toolbar);
 
-        //get the reference to itself (activity)
         thisActivity = this;
 
-        // Get a support ActionBar corresponding to this toolbar
-        ActionBar ab = getSupportActionBar();
-        ab.setTitle("New List");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // Enable the Up button
-        ab.setDisplayHomeAsUpEnabled(true);
+        //Get the List that is relevant
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if(extras == null) {
+                System.err.println("Edit List Activity cannot proceed without LIST_ID");
+                System.exit(0);
+            } else {
+                currentListID = extras.getInt("X_LIST_ID");
+            }
+        } else {
+            currentListID= (int) savedInstanceState.getSerializable("X_LIST_ID");
+        }
 
+        //get the List with its Tags
+        myRep = new LocalDataRepository(this);
+        currentList = myRep.getListWithTagsByID(currentListID);
+
+        //set the title
+        toolbar.setTitle("Edit: " + currentList.getXListModel().getXListTitle());
+
+        //set the TextFields
+        ((TextView)findViewById(R.id.xlist_title_input)).setText(currentList.getXListModel().getXListTitle());
+        ((TextView)findViewById(R.id.xlist_short_desc_input)).setText(currentList.getXListModel().getXListShortDescription());
+        ((TextView)findViewById(R.id.xlist_long_desc_input)).setText(currentList.getXListModel().getXListLongDescription());
+
+        //inflate the Tags
+        //since everything is tied to the tagView, we can have different onClickListeners for different type of TagViews
+        insertPoint = (ViewGroup) findViewById(R.id.xList_tags_tagsList_view);
+        for (XTagModel tempTag : currentList.getXTagModelList()){
+            final View tagView = getLayoutInflater().inflate(R.layout.tag_element, null);
+
+            TextView tagTextView = (TextView) tagView.findViewById(R.id.tag_name_text);
+            tagTextView.setText("#"+tempTag.getXTagName());
+
+            //This is, so when the X is clicked the tag is put on the List of Tags to be permanently removed
+            final int tagID = tempTag.getXTagID();
+            ImageButton tagImgButton = (ImageButton) tagView.findViewById(R.id.tag_delete_button);
+            tagImgButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view2) {
+                    tempTagListDeleted.add(tagID);
+                    ViewGroup parent = (ViewGroup) findViewById(R.id.xList_tags_tagsList_view);
+                    parent.removeView(tagView);
+                }
+            });
+
+            insertPoint.addView(tagView, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        }
+
+        //the text typed in the addTag TextField
         tagEditText = (EditText)findViewById(R.id.xList_tag_input_field);
 
+        //what happens to newly inserted Tags
         //The Tag Add Button
         Button tagAddButton = (Button) findViewById(R.id.xList_tag_input_button);
         tagAddButton.setOnClickListener(new View.OnClickListener() {
@@ -64,32 +112,29 @@ public class XListEditActivity extends AppCompatActivity {
             public void onClick(View view) {
                 //get the tagText
                 //if its not empty, add it to the temporary List
-                if (!tagEditText.getText().toString().equals("")){
+                if (!tagEditText.getText().toString().equals("")) {
                     final String tempTempStr = tagEditText.getText().toString();
-                    tempTagList.add(tempTempStr);
+                    tempTagListNew.add(tempTempStr);
 
                     //All this to put a visual representation of the tag in the view
                     final View tagView = getLayoutInflater().inflate(R.layout.tag_element, null);
 
                     //The TextView of the TagView is filled here
                     TextView tagTextView = (TextView) tagView.findViewById(R.id.tag_name_text);
-                    tagTextView.setText("#"+tagEditText.getText().toString());
+                    tagTextView.setText("#" + tagEditText.getText().toString());
 
                     //This is, so when the X is clicked the tag is removed
                     ImageButton tagImgButton = (ImageButton) tagView.findViewById(R.id.tag_delete_button);
-                    tagImgButton.setOnClickListener(new View.OnClickListener(){
+                    tagImgButton.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View view2) {
-                            tempTagList.remove(tempTempStr);
+                            tempTagListNew.remove(tempTempStr);
                             ViewGroup parent = (ViewGroup) findViewById(R.id.xList_tags_tagsList_view);
                             parent.removeView(tagView);
-                            //parent.removeViewAt(0); this works as expected
-                            //v.setVisibility(View.GONE);
                         }
                     });
 
                     //The tagView is inserted into the LinearLayout
-                    ViewGroup insertPoint = (ViewGroup) findViewById(R.id.xList_tags_tagsList_view);
                     insertPoint.addView(tagView, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
 
                     tagEditText.setText("");
@@ -113,36 +158,31 @@ public class XListEditActivity extends AppCompatActivity {
                 String tempShortDesc = ((TextView)findViewById(R.id.xlist_short_desc_input)).getText().toString();
                 String tempLongDesc = ((TextView)findViewById(R.id.xlist_long_desc_input)).getText().toString();
 
-                List<XTagModel> tagList = new ArrayList<XTagModel>();
+                List<XTagModel> newTagList = new ArrayList<XTagModel>();
 
+                //update the necessary values
+                currentList.getXListModel().setXListTitle(tempTitle);
+                currentList.getXListModel().setXListShortDescription(tempShortDesc);
+                currentList.getXListModel().setXListLongDescription(tempLongDesc);
 
-                LocalDataRepository myRep = new LocalDataRepository(view.getContext());
-                if (myRep == null) {
-                    System.exit(0);
-                }
-                //TODO: create xtagmodels with appropriate id and insert them as well
-                long listID = myRep.insertList(new XListModel(tempTitle,tempShortDesc,tempLongDesc,myRep.getListCount()+1));
-
-                //ATTENTION: THIS COULD BE A MAJOR MISTAKE CONVERTING LONG TO INT,
-                // however, the long value should be the primary key, thus it should not give a conversion problem since primary keyss are int
-                if (Integer.MAX_VALUE < listID){
-                    System.err.println("The long value can apperently not be the listID, what happened?");
-                    System.exit(0);
-                }
+                //update the Room DataBase
+                myRep.updateList(currentList.getXListModel());
 
                 //making XTagModels from the Strings
-                for (String tempStr : tempTagList) {
-                    tagList.add(new XTagModel((int) listID, tempStr));
+                for (String tempStr : tempTagListNew) {
+                    newTagList.add(new XTagModel(currentList.getXListModel().getXListID(), tempStr));
                 }
 
                 //inserting the created tag list
-                myRep.insertTags(tagList);
+                myRep.insertTags(newTagList);
+
+                //now delete the appropriate Tags
+                myRep.deleteTagsByID(tempTagListDeleted);
 
                 //return to parent activity
                 NavUtils.navigateUpFromSameTask(thisActivity);
             }
         });
-
     }
 
     @Override
@@ -150,44 +190,43 @@ public class XListEditActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             // Respond to the action bar's Up/Home button
             case android.R.id.home:
-                //retrieving the inputs from all the TextViews
-                String tempTitle = ((TextView)findViewById(R.id.xlist_title_input)).getText().toString();
-                String tempShortDesc = ((TextView)findViewById(R.id.xlist_short_desc_input)).getText().toString();
-                String tempLongDesc = ((TextView)findViewById(R.id.xlist_long_desc_input)).getText().toString();
-
-                //if there is nothing entered so far
-                if (tempTitle.equals("") && tempShortDesc.equals("") && tempLongDesc.equals("")){
-                    //exit without saving anything
-                    NavUtils.navigateUpFromSameTask(thisActivity);
-                    return true;
-                } else {
-                    //FROM HERE ON ITS THE ALERT DIALOG
-                    AlertDialog.Builder builder;
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                        builder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
-                    } else {
-                        builder = new AlertDialog.Builder(this);
-                    }
-                    builder.setTitle("Exit whitout save")
-                            .setMessage("Are you sure you want to exit without saving the list?")
-                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    //exit without saving anything
-                                    NavUtils.navigateUpFromSameTask(thisActivity);
-                                }
-                            })
-                            .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    // do nothing
-                                }
-                            })
-                            .setIcon(android.R.drawable.ic_dialog_alert)
-                            .show();
-                    //new ConfirmationDialog().show(getSupportFragmentManager(), "confirmation_dialog");
-                    return true;
-                }
+                //return to mainActivity
+                returnToMainActivity();
+                return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    //what happens when back button is pressed
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)  {
+        if (keyCode == KeyEvent.KEYCODE_BACK ) {
+            //return to mainActivity
+            returnToMainActivity();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void returnToMainActivity(){
+        //FROM HERE ON ITS THE ALERT DIALOG
+        AlertDialog.Builder builder;
+        builder = new AlertDialog.Builder(this, R.style.AppCompatAlertDialogStyle);
+        builder.setTitle("Exit whitout saving?");
+        builder.setMessage("Are you sure you want to exit without saving the list?");
+        builder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                //exit without saving anything
+                NavUtils.navigateUpFromSameTask(thisActivity);
+            }
+        });
+        builder.setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // do nothing
+            }
+        });
+        builder.show();
     }
 
 }
