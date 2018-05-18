2d1ebd779a7d05fad40817271d8f1686beb445ab
AaronSpieler <spieleraaron@protonmail.com>
2018-05-19 01:44:09 +0200

    Uploading Models?

---
+++ app/src/main/java/com/whynoteasy/topxlist/objects/XElemModel.java
@@ -0,0 +1,141 @@
+package com.whynoteasy.topxlist.objects;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.Index;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by Whatever on 15.11.2017.
+ * Main Purpose: Represents Lists Elements, Defines through Room database tables
+ */
+
+@Entity(tableName = "elements",
+        indices = @Index(value = "list_id", name = "elements_list_idx"),
+        foreignKeys = @ForeignKey(entity = XListModel.class,
+                parentColumns = "list_id",
+                childColumns = "list_id",
+                onDelete = ForeignKey.CASCADE,
+                onUpdate = ForeignKey.NO_ACTION
+        ))
+
+public class XElemModel implements Comparable<XElemModel>{
+    //Constants
+    @Ignore
+    public static final int xETlength = 255; //title length
+    @Ignore
+    public static final int xEDlength = 8192; //description/Body length
+
+    //Attributes
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "element_id")
+    private int xElemID;
+
+    @ColumnInfo(name = "list_id")
+    private final int xListIDForeign;
+
+    @ColumnInfo(name = "element_name")
+    private String xElemTitle;
+
+    @ColumnInfo(name = "element_desc")
+    private String xElemDescription;
+
+    @ColumnInfo(name = "element_num")
+    private int xElemNum;
+
+    @ColumnInfo(name = "marked_status")
+    private boolean xElemMarked;
+
+    //so far not in use
+    @ColumnInfo(name = "media_id")
+    private int xMediaID;
+
+    //Constructor
+    @Ignore
+    public XElemModel(int xListIDForeign, String xElemTitle, String xElemDescription, int xElemNum) {
+        this.xListIDForeign = xListIDForeign;
+        this.xElemTitle = xElemTitle;
+        this.xElemDescription = xElemDescription;
+        this.xElemNum = xElemNum;
+        this.xElemMarked = false;
+        this.xMediaID = 0;
+    }
+
+    public XElemModel(int xListIDForeign, String xElemTitle, String xElemDescription, int xElemNum, int xMediaID) {
+        this.xListIDForeign = xListIDForeign;
+        this.xElemTitle = xElemTitle;
+        this.xElemDescription = xElemDescription;
+        this.xElemNum = xElemNum;
+        this.xElemMarked = false;
+        this.xMediaID = xMediaID;
+    }
+
+    //Getters and Setters
+
+    public int getXElemID() {
+        return xElemID;
+    }
+
+    public void setXElemID(int xElemID) {
+        this.xElemID = xElemID;
+    }
+
+    public String getXElemTitle() {
+        return xElemTitle;
+    }
+
+    public void setXElemTitle(String xElemTitle) {
+        this.xElemTitle = xElemTitle;
+    }
+
+    public String getXElemDescription() {
+        return xElemDescription;
+    }
+
+    public void setXElemDescription(String xElemDescription) {
+        this.xElemDescription = xElemDescription;
+    }
+
+    public int getXElemNum() {
+        return xElemNum;
+    }
+
+    public void setXElemNum(int xElemNum) {
+        this.xElemNum = xElemNum;
+    }
+
+    public int getXListIDForeign() {
+        return xListIDForeign;
+    }
+
+    public boolean isXElemMarked() {
+        return xElemMarked;
+    }
+
+    public void setXElemMarked(boolean xElemMarked) {
+        this.xElemMarked = xElemMarked;
+    }
+
+    public int getXMediaID() {
+        return xMediaID;
+    }
+
+    public void setXMediaID(int xMediaID) {
+        this.xMediaID = xMediaID;
+    }
+
+    //Other Methods
+
+    @Override
+    public int compareTo(@NonNull XElemModel xElemModel) {//to be able to order them should it be necessary
+        return (this.getXElemNum() - xElemModel.getXElemNum());
+    }
+
+    public void negateMarked(){
+        xElemMarked = !xElemMarked;
+    }
+}
---
+++ app/src/main/java/com/whynoteasy/topxlist/objects/XTagModel.java
@@ -0,0 +1,62 @@
+package com.whynoteasy.topxlist.objects;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.Index;
+import android.arch.persistence.room.PrimaryKey;
+
+/**
+ * Created by Whatever on 15.11.2017.
+ * To use with the VMMV pattern
+ * Main Purpose: Represents Tags, Defines through Room database tables
+ */
+
+@Entity(tableName = "tags",
+        indices = @Index(value = "list_id", name = "tags_list_idx"), //foreign keys should be indexed
+        foreignKeys = @ForeignKey(entity = XListModel.class,
+            parentColumns = "list_id",
+            childColumns = "list_id",
+            onDelete = ForeignKey.CASCADE,
+            onUpdate = ForeignKey.NO_ACTION
+        ))
+
+public class XTagModel {
+    //Attributes
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "tag_id")
+    private int xTagID;
+
+    @ColumnInfo(name = "list_id")
+    private final int xListIDForeign;
+
+    @ColumnInfo(name = "tag_name")
+    private final String xTagName;
+
+    //Constructor
+
+    public XTagModel(int xListIDForeign, String xTagName) {
+        this.xListIDForeign = xListIDForeign;
+        this.xTagName = xTagName;
+    }
+
+    //Getters and Setters
+
+    public String getXTagName() {
+        return xTagName;
+    }
+
+    public int getXTagID() {
+        return xTagID;
+    }
+
+    public void setXTagID(int xTagID) {
+        this.xTagID = xTagID;
+    }
+
+    public int getXListIDForeign() {
+        return xListIDForeign;
+    }
+
+}
